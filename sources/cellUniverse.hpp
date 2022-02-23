866a54d71add6efe4723b028b4c8b4fdb1bd56a8
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-02-23 21:59:06 +0000

    added back cellUniverse

---
+++ sources/cellUniverse.hpp
@@ -0,0 +1,16 @@
+// These can be modified
+// -----------------------
+const int VERSE_W = 5;
+const int VERSE_H = 5;
+// -----------------------
+
+
+
+// Performs one time step of the universe
+void step();
+
+// Set the current universe
+void setCellUniverse(const int (*const provided)[VERSE_W][VERSE_H]);
+
+// Get a pointer to a pointer to the current cell universe
+const int (*const *getCellUniverse())[VERSE_W][VERSE_H];
