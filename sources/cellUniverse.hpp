bcac45447ea347bab4afd25be948691b3eede70f
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-09 21:36:21 +0100

    added field_vector and getFieldUniverse()

--- sources/cellUniverse.hpp
+++ sources/cellUniverse.hpp
@@ -4,6 +4,11 @@ const int VERSE_W = 200;
 const int VERSE_H = 200;
 // -----------------------
 
+typedef struct field_vector {
+    int x;
+    int y;
+} field_vector;
+
 // Performs one time step of the universe
 void step();
 
@@ -12,3 +17,6 @@ void setCellUniverse(int (&x)[VERSE_W][VERSE_H]);
 
 // Get a pointer to a pointer to the current cell universe
 const int (&getCellUniverseRef())[VERSE_W][VERSE_H];
+
+// Get a pointer to a pointer to the current field universe
+const field_vector (&getFieldUniverse())[VERSE_W][VERSE_H];
