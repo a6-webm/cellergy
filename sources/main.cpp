d1943f488bde427f5b22f7ee9ec097b4551bdf40
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-10 16:54:38 +0100

    added step controls and draw scaling

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <random>
 
-#define SCREEN_WIDTH (900)
-#define SCREEN_HEIGHT (802)
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (800)
 
 #define WINDOW_TITLE "Window title"
 
@@ -16,12 +16,13 @@ int main()
 {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<int> int_dist(0,1);
+//    std::uniform_int_distribution<int> int_dist(0,1);
+    std::binomial_distribution<int> binom_dist(1,.5);
 
     int verseStart[VERSE_W][VERSE_W];
     for (auto & x : verseStart)
         for (int & y : x)
-            y = int_dist(rng);
+            y = binom_dist(rng);
 
     setCellUniverse(verseStart);
     cellUniverseDisplayer_debug();
@@ -33,59 +34,72 @@ void cellUniverseDisplayer_debug() // TODO test
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(30);
 
-    RenderTexture2D target = LoadRenderTexture(VERSE_W*4, VERSE_H*4);
+    RenderTexture2D target = LoadRenderTexture(VERSE_W*2, VERSE_H*2);
+
+    bool init = true;
 
     while (!WindowShouldClose())
     {
-        BeginTextureMode(target);
+        if (IsKeyPressed(KEY_SPACE) || IsKeyDown(KEY_N)) {
+            step();
 
-        ClearBackground(BLACK);
+            BeginTextureMode(target);
 
-        const int (&verseView)[VERSE_W][VERSE_W] = getCellUniverseRef();
-        const field_vector (&fieldView)[VERSE_W][VERSE_H] = getFieldUniverse();
-        for (int x = 0; x < VERSE_W; x++)
-            for (int y = 0; y < VERSE_H; y++)
-            {
-                int twox = 2*x;
-                int twoy = 2*y;
+            ClearBackground(BLACK);
 
-                int repeat_val = 35;
-                double v = (double)verseView[x][y] / repeat_val;
-                unsigned char r = round(sin(2 * PI * v + 2)) * 127 + 128;
-                unsigned char g = round(sin(2 * PI * v + 0)) * 127 + 128;
-                unsigned char b = round(sin(2 * PI * v + 4)) * 127 + 128;
+            const int (&verseView)[VERSE_W][VERSE_W] = getCellUniverseRef();
+            const field_vector (&fieldView)[VERSE_W][VERSE_H] = getFieldUniverse();
+            for (int x = 0; x < VERSE_W; x++)
+                for (int y = 0; y < VERSE_H; y++)
+                {
+                    int twox = 2*x;
+                    int twoy = 2*y;
 
-                DrawRectangle(twox,twoy,2,2,{r,g,b,255});
+                    int repeat_val = 5;
+                    double v = (double)verseView[x][y] / repeat_val;
+                    unsigned char r = round(sin(2 * PI * v + 2)) * 127 + 128;
+                    unsigned char g = round(sin(2 * PI * v + 0)) * 127 + 128;
+                    unsigned char b = round(sin(2 * PI * v + 4)) * 127 + 128;
 
-                field_vector field = fieldView[x][y];
-                repeat_val = 200;
-                v = ((double)abs(field.x) + abs(field.y)) / repeat_val;
-                r = round(sin(2 * PI * v + 2)) * 127 + 128;
-                g = round(sin(2 * PI * v + 0)) * 127 + 128;
-                b = round(sin(2 * PI * v + 4)) * 127 + 128;
+                    DrawRectangle(twox,twoy,2,2,{r,g,b,255});
 
-                if (field.x == 0 && field.y == 0) {
-                    DrawPixel(twox,twoy,{0,0,0,255});
-                    DrawPixel(twox,twoy,{0,0,0,255});
-                } else {
-                    int dx = field.x > 0 ? 1 : 0;
-                    int dy = field.y > 0 ? 1 : 0;
-                    int wid = field.x == 0 ? 2 : 1;
-                    int hei = field.y == 0 ? 2 : 1;
-                    DrawRectangle(twox+dx,twoy+dy,wid,hei,{r,g,b,255});
+                    field_vector field = fieldView[x][y];
+                    repeat_val = 200;
+                    v = ((double)abs(field.x) + abs(field.y)) / repeat_val;
+                    r = round(sin(2 * PI * v + 2)) * 127 + 128;
+                    g = round(sin(2 * PI * v + 0)) * 127 + 128;
+                    b = round(sin(2 * PI * v + 4)) * 127 + 128;
+
+                    if (field.x == 0 && field.y == 0) {
+                        DrawPixel(twox,twoy,{0,0,0,255});
+                        DrawPixel(twox,twoy,{0,0,0,255});
+                    } else {
+                        int dx = field.x > 0 ? 1 : 0;
+                        int dy = field.y > 0 ? 1 : 0;
+                        int wid = field.x == 0 ? 2 : 1;
+                        int hei = field.y == 0 ? 2 : 1;
+                        DrawRectangle(twox+dx,twoy+dy,wid,hei,{r,g,b,255});
+                    }
                 }
-            }
-        EndTextureMode();
+            EndTextureMode();
+        }
 
         BeginDrawing();
 
-        DrawTextureRec(target.texture, (Rectangle) { 0, 0, (float)target.texture.width, (float)-target.texture.height }, (Vector2) { 0, 0 }, WHITE);
+        ClearBackground(WHITE);
+        DrawTextureNPatch(
+                target.texture,
+                {
+                        {0,0,(float)target.texture.width,(float)-target.texture.height},
+                        0,0,0,0,0
+                        },
+                { 0, 0, 800, 800 },
+                { 0, 0 },
+                0,
+                WHITE
+        );
 
         EndDrawing();
-
-        while(!IsKeyPressed(KEY_SPACE)) {}
-
-        step();
     }
 
     CloseWindow();
