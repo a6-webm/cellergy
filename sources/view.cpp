20867f740283d414205730fb50eca26dcffbd08b
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-19 15:03:17 +0100

    implemented displaying frData.view not rendering the entire verse

--- sources/view.cpp
+++ sources/view.cpp
@@ -2,21 +2,34 @@
 #include "cellUniverse.hpp"
 #include "raylib.h"
 #include <cmath>
+#include <iostream>
 
 RenderTexture2D universeTexture;
-
-void drawUI(frame_data data);
+template <typename T>
+struct Box {T left; T right; T top; T bottom;};
+Box<unsigned int> lastDraw = {0,0,0,0};
+Box<float> lastRenderedView = {0, 0, 0, 0};
 
 void reDrawUniverse(frame_data frData) {
-    universeTexture = LoadRenderTexture(frData.draw.right-frData.draw.left, frData.draw.bottom-frData.draw.top);
+    lastRenderedView.left = frData.view.x - (frData.renderRange * frData.view.width + 1);
+    lastRenderedView.right = frData.view.x + ((frData.renderRange + 1) * frData.view.width + 1);
+    lastRenderedView.top = frData.view.y - (frData.renderRange * frData.view.height + 1);
+    lastRenderedView.bottom = frData.view.y + ((frData.renderRange + 1) * frData.view.height + 1);
+
+    lastDraw.left = std::min((unsigned int)VERSE_W-1, static_cast<unsigned int>(std::max(0.0f, lastRenderedView.left)));
+    lastDraw.right = std::min((unsigned int)VERSE_W-1, static_cast<unsigned int>(std::max(0.0f, lastRenderedView.right)));
+    lastDraw.top = std::min((unsigned int)VERSE_H-1, static_cast<unsigned int>(std::max(0.0f, lastRenderedView.top)));
+    lastDraw.bottom = std::min((unsigned int)VERSE_H-1, static_cast<unsigned int>(std::max(0.0f, lastRenderedView.bottom)));
+
+    universeTexture = LoadRenderTexture(lastDraw.right-lastDraw.left+1, lastDraw.bottom-lastDraw.top+1);
 
     BeginTextureMode(universeTexture);
 
     ClearBackground(GRAY);
 
     const int (&verseView)[VERSE_W][VERSE_W] = getCellUniverseRef();
-    for (int x = frData.draw.left; x <= frData.draw.right; x++)
-        for (int y = frData.draw.top; y <= frData.draw.bottom; y++)
+    for (int x = lastDraw.left; x <= lastDraw.right; x++)
+        for (int y = lastDraw.top; y <= lastDraw.bottom; y++)
         {
             double v;
             unsigned char r;
@@ -32,7 +45,7 @@ void reDrawUniverse(frame_data frData) {
                 g = round(sin(2 * PI * v + 0)) * 127 + 128;
                 b = round(sin(2 * PI * v + 4)) * 127 + 128;
             }
-            DrawPixel(x, y, {r, g, b, 255});
+            DrawPixel(x - lastDraw.left, y - lastDraw.top, {r, g, b, 255});
         }
     EndTextureMode();
 }
@@ -42,21 +55,32 @@ void drawUI(frame_data data) {
 }
 
 void drawFrame(frame_data frData){
-    if (frData.reDrawUniverse) reDrawUniverse(frData);
-
+    if (
+            frData.universeStepped
+         || frData.view.x < lastRenderedView.left
+         || frData.view.y < lastRenderedView.top
+         || frData.view.x + frData.view.width > lastRenderedView.right
+         || frData.view.y + frData.view.height > lastRenderedView.bottom
+       ) {
+        reDrawUniverse(frData);
+        std::cout << "Universe redrawn" << std::endl;
+    }
     BeginDrawing();
     ClearBackground(WHITE); //TODO keep in mind, this line only needed so empty texture doesn't flash screen black and white
-    DrawTextureNPatch(
+    DrawTexturePro(
             universeTexture.texture,
+            {0,0,(float)universeTexture.texture.width,-(float)universeTexture.texture.height},
             {
-                    {0,0,(float)universeTexture.texture.width,(float)-universeTexture.texture.height},
-                    0,0,0,0,0
-            },
-            { 0, 0, (float)frData.screen_width, (float)frData.screen_height },
+                -(frData.view.x - lastDraw.left)*(frData.screen_width/frData.view.width),
+                -(frData.view.y - lastDraw.top)*(frData.screen_width/frData.view.width),
+                (float)frData.screen_width * universeTexture.texture.width/frData.view.width,
+                (float)frData.screen_height * universeTexture.texture.height/frData.view.height
+                },
             { 0, 0 },
             0,
             WHITE
     );
     drawUI(frData);
+
     EndDrawing();
 }
