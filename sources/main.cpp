c213fe9567ddb60568ecdce0b2206763f8d76729
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-19 17:25:35 +0100

    added lerp to camera and centered zoom

--- sources/main.cpp
+++ sources/main.cpp
@@ -48,26 +48,50 @@ void mvcLoop() {
     typedef struct Player {
         int x;
         int y;
-
+        int moveSpeed;
     } Player;
+    Player player = {100,100, 1};
+    Vector2 viewTarget;
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_W)) frData.view.y -= .5f;
-        if (IsKeyDown(KEY_S)) frData.view.y += .5f;
-        if (IsKeyDown(KEY_A)) frData.view.x -= .5f;
-        if (IsKeyDown(KEY_D)) frData.view.x += .5f;
+        if (IsKeyDown(KEY_W)) player.y -= player.moveSpeed;
+        if (IsKeyDown(KEY_S)) player.y += player.moveSpeed;
+        if (IsKeyDown(KEY_A)) player.x -= player.moveSpeed;
+        if (IsKeyDown(KEY_D)) player.x += player.moveSpeed;
 
+        float zoomRate = 0.03f;
         if (IsKeyDown(KEY_Q)) {
-            frData.view.width -= 2.0f;
-            frData.view.height -= 2.0f;
-            if (frData.view.width < 0) frData.view.width = 0.0f;
-            if (frData.view.height < 0) frData.view.height = 0.0f;
+            frData.view.width -= frData.view.width*zoomRate;
+            frData.view.height = frData.view.width*SCREEN_HEIGHT/SCREEN_WIDTH;
+            if (frData.view.width < 0)
+                frData.view.width = frData.view.height = 0.0f;
+            else {
+                frData.view.x += frData.view.width*zoomRate/2;
+                frData.view.y += frData.view.height*zoomRate/2;
+            }
         }
         if (IsKeyDown(KEY_E)) {
-            frData.view.width += 2.0f;
-            frData.view.height += 2.0f;
+            frData.view.width += frData.view.width*zoomRate;
+            frData.view.height = frData.view.width*SCREEN_HEIGHT/SCREEN_WIDTH;
+            frData.view.x -= frData.view.width*zoomRate/2;
+            frData.view.y -= frData.view.height*zoomRate/2;
         }
+
+        viewTarget.x = player.x - frData.view.width/2;
+        viewTarget.y = player.y - frData.view.height/2;
+
+
+        float lerpRate = GetFrameTime()*4.0f;
+        float lerpThresh = 0.01f;
+        if (std::abs(viewTarget.x - frData.view.x) < lerpThresh) {
+            frData.view.x = viewTarget.x;
+        } else
+            frData.view.x += lerpRate*(viewTarget.x - frData.view.x);
+        if (std::abs(viewTarget.y - frData.view.y) < lerpThresh) {
+            frData.view.y = viewTarget.y;
+        } else
+            frData.view.y += lerpRate*(viewTarget.y - frData.view.y);
 
         frData.universeStepped = false;
         if (IsKeyPressed(KEY_SPACE) || IsKeyDown(KEY_N)) {
