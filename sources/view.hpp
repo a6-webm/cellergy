f091e64f4e5c4388bb2a61a59dedfd7b7d141ccf
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-19 15:02:01 +0100

    removed draw, added renderRange

--- sources/view.hpp
+++ sources/view.hpp
@@ -5,10 +5,9 @@
 
 typedef struct frame_data {
     unsigned int screen_width, screen_height;
-    typedef struct Box {unsigned int left; unsigned int right; unsigned int top; unsigned int bottom;} Box;
-    Box draw;
-    bool reDrawUniverse;
     Rectangle view;
+    float renderRange;
+    bool universeStepped;
     unsigned char simState;
 } frame_data;
 
