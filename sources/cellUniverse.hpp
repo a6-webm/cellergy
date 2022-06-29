14fe7b64f0a4a0f41e22367e2b70722c02f1325d
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-29 16:17:43 -0500

    added Player struct

--- sources/cellUniverse.hpp
+++ sources/cellUniverse.hpp
@@ -5,8 +5,16 @@ const int VERSE_W = 400;
 const int VERSE_H = 400;
 // -----------------------
 
+typedef struct Player {
+    int x;
+    int y;
+    int val;
+    bool sim;
+    int energy;
+} Player;
+
 // Performs one time step of the universe
-void step();
+Player step(Player player);
 
 // Set the current universe, provided array must not disappear
 void setCellUniverse(int (&x)[VERSE_W][VERSE_H]);
