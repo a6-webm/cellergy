0709a3402338e0ec4687c3c1d734d7fa98e4bd48
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-06-29 16:16:15 -0500

    game of life on cell value 2, and player moves with prototype of energy mechanic

--- sources/cellUniverse.cpp
+++ sources/cellUniverse.cpp
@@ -15,6 +15,9 @@ int sub_cell_values_2[VERSE_W][VERSE_H];
 
 const int CELL_MAX = INT_MAX / 2;
 
+Player prevPlayer;
+Player currPlayer;
+
 int (&currVerse())[VERSE_W][VERSE_H] {
     if (isUniverse1)
         return sub_cell_values_1;
@@ -38,27 +41,35 @@ void sim_cell(int x, int y) {
             if (x+d_x >= VERSE_W) dd_x = -x;
             if (y+d_y < 0) dd_y = VERSE_H-1 - y;
             if (y+d_y >= VERSE_H) dd_y = -y;
-            if (prevVerse()[x+dd_x][y+dd_y] > 0) ++aliveCount;
+            if (prevVerse()[x+dd_x][y+dd_y] == 2) ++aliveCount;
         }
 
-    if (prevVerse()[x][y] == 0)
-    {
-        if (aliveCount == 3) currVerse()[x][y] = 1;
-    } else
-    {
-        if (aliveCount == 2 || aliveCount == 3)
-        {
-            if (prevVerse()[x][y] == CELL_MAX - 1)
-                currVerse()[x][y] = 1;
-            else
-                currVerse()[x][y] = prevVerse()[x][y] + 1;
+    if (prevVerse()[x][y] == 0) {
+        if (aliveCount == 3) {
+            currVerse()[x][y] += 2;
+            currPlayer.energy -= 2;
         }
+    } else if (aliveCount == 2 || aliveCount == 3) {
+        currVerse()[x][y] += 2;
+    } else {
+        currPlayer.energy += 2;
     }
 }
 
-void step() {
+void sim_player() {
+    if (currPlayer.sim) {
+        // TODO implement the player being able to act like a simulated cell
+    } else {
+        currPlayer.energy += prevPlayer.val - currPlayer.val + currVerse()[currPlayer.x][currPlayer.y];
+        currVerse()[currPlayer.x][currPlayer.y] = currPlayer.val;
+    }
+}
+
+Player step(Player newPlayer) {
     // Swap current universe
     isUniverse1 = !isUniverse1;
+    prevPlayer = currPlayer;
+    currPlayer = newPlayer;
 
     // Clear new universe
     memset((void *) currVerse(), 0, sizeof(currVerse()));
@@ -66,7 +77,12 @@ void step() {
     // Simulate new cells
     for (int i_x = 0; i_x < VERSE_W; ++i_x)
         for (int i_y = 0; i_y < VERSE_H; ++i_y)
-            sim_cell(i_x,i_y);
+            if (i_x != prevPlayer.x || i_y != prevPlayer.y)
+                sim_cell(i_x,i_y);
+
+    sim_player();
+
+    return currPlayer;
 }
 
 void setCellUniverse(int (&x)[VERSE_W][VERSE_H]) {
